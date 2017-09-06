ea287d30f6330ec7231150f37113e520662d5e4e
dSecret <ajrathore786@gmail.com>
2017-09-07 01:38:06 +0530

    first commmit

---
+++ src/components/AuthSuccess.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div>
+        <button @click="logOut">logout</button>
+    </div>
+    <div>
+        <img :src="photo" style='height: 120px'/> <br>
+        <p>{{name}}</p>
+        <p>{{email}}</p>
+        <p>{{userId}}</p>
+        <pre>{{user}}</pre>
+      </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import firebase from 'firebase'
+
+export default {
+  data() {
+    return {
+      photo: '',
+      userId: '',
+      name: '',
+      email: '',
+      user: {},
+      roomDetails: [],
+      test_room: {},
+      test_id: ''
+    }
+  },
+  created() {
+    var vm = this
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        vm.user = user;
+        vm.name = vm.user.displayName;
+        vm.email = vm.user.email;
+        vm.photo = vm.user.photoURL;
+        vm.userId = vm.user.uid;
+      }
+    });
+
+    axios.get('http://localhost:3000/rooms')
+      .then((response) => {
+        this.roomDetails = response.data;
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  },
+  methods: {
+    logOut() {
+      firebase.auth().signOut();
+    },
+  },
+};
+</script>
---
+++ src/helpers/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: "AIzaSyAt_piQHTaz_eyDDjcYfnUM9RUYWy5dFpI",
+  authDomain: "fir-auth-dabd5.firebaseapp.com",
+  databaseURL: "https://fir-auth-dabd5.firebaseio.com",
+  projectId: "fir-auth-dabd5",
+  storageBucket: "fir-auth-dabd5.appspot.com",
+  messagingSenderId: "1003786982889"
+};
