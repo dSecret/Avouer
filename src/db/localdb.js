9a7f672de75f4a43673fa448e1d6c3e4c31a7f4d
Boot-Error <booterror99@gmail.com>
2017-09-08 19:47:29 +0530

    implementing local store

---
+++ src/db/localdb.js
@@ -0,0 +1,24 @@
+import dexie from 'dexie'
+
+const db = new Dexie('avouer-db')
+
+// create a db if non existant
+db.version(1).stores({
+	posts: 'id, title, content, link, created'
+})
+
+db.open()
+
+export default () => {
+	getPost (id) {
+		db.posts
+			.where('id')
+			.equals(id)
+			.then(post => {
+				return post
+			})
+	},
+	savePost (post) {
+		db.posts.
+	}
+}
